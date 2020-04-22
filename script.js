a0b421069ee44429a9d578a14a9ca36c88658124
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-04-22 19:37:16 +0500

    Make it adaptive
    Make it the full adaptive.  In this commit — mobile version

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+//header nav pop-up menu for screen size less than 991 px;
 function someFunc() {
-    // Do it if the screen size is less than 1000
+    // Do it if the screen size is less than 991
     var w = window.innerWidth;
-    if (w < 1000) {
+    if (w < 991) {
         var menu_icon = document.getElementById("menu_icon");
         var side_nav = document.getElementById("side_nav");
         var header = document.getElementById("header")
